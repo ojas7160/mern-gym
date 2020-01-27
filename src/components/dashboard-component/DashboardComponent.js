91289b03bfcb8f795d56b4358cf070df6613a7d6
Ojaswi Wadhwa <ojaswi.wadhwa@azimuthgrc.com>
2020-01-27 19:41:55 +0530

    dashboard routing

---
+++ src/components/dashboard-component/DashboardComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { render } from '@testing-library/react';
+
+const DashboardComponent = (props) => (
+  <div>
+    <h1>Hello dashboard</h1>
+  </div>
+)
+
+export default DashboardComponent;
